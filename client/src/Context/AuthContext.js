0ea1d3a9d5f5e1b8400fcc1f116c17855fec0b5d
Aadish Jain <jain.aadishj@gmail.com>
2024-11-17 10:16:49 +0530

    Fixed import bug

--- client/src/Context/AuthContext.js
+++ client/src/Context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import axios from "../../axiosInstance";
+import axios from "../axiosInstance";
 
 const AuthContext = createContext();
 
